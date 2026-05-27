44abfc5f14c3218638656c59b361e8cccd486332
abhaypandit123 <abhaytech12cy@gmail.com>
2026-05-27 09:38:12 +0530

    add

---
+++ 01_Python_Basic/01_python_basic.py
@@ -0,0 +1,18 @@
+print()
+print("I am abhay.")
+print("How are you?")
+print("I am fine.")
+print("4")
+print(4)
+# if(a<3):
+#   statement1
+#  statement2
+'''this is a                                                          
+comment
+'''
+age=4 #integer
+name="harry" #string
+y="hellow" #string
+cgpa=9.5 #float
+
+# ab$$c invalid variable name
